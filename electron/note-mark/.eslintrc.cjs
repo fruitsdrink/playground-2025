cfca00e7031736a1e98c722dbac9f9671883b514
fruitsdrink <fruitsdrink@126.com>
2024-12-27 15:45:18 +0800

    note-mark: init

--- electron/note-mark/.eslintrc.cjs
+++ electron/note-mark/.eslintrc.cjs
@@ -1,3 +1,5 @@
+const { rules } = require('eslint-plugin-react')
+
 module.exports = {
   extends: [
     'eslint:recommended',
@@ -5,5 +7,9 @@ module.exports = {
     'plugin:react/jsx-runtime',
     '@electron-toolkit/eslint-config-ts/recommended',
     '@electron-toolkit/eslint-config-prettier'
-  ]
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off'
+  }
 }
