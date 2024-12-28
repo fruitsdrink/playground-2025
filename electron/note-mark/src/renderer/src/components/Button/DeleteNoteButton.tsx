76f9e446c72bccc0df83c4d832dc2a7d6e404726
fruitsdrink <fruitsdrink@126.com>
2024-12-28 11:12:52 +0800

    note-mark: 完成新增和删除操作

--- electron/note-mark/src/renderer/src/components/Button/DeleteNoteButton.tsx
+++ electron/note-mark/src/renderer/src/components/Button/DeleteNoteButton.tsx
@@ -1,9 +1,16 @@
 import { ActionButton, ActionButtonProps } from '@/components'
+import { deleteNoteAtom } from '@/store'
+import { useSetAtom } from 'jotai'
 import { FaRegTrashAlt } from 'react-icons/fa'
 
 export const DeleteNoteButton = ({ ...props }: ActionButtonProps) => {
+  const deleteNote = useSetAtom(deleteNoteAtom)
+  const handleDelete = () => {
+    deleteNote()
+  }
+
   return (
-    <ActionButton {...props}>
+    <ActionButton {...props} onClick={handleDelete}>
       <FaRegTrashAlt className="w-4 h-4 to-zinc-300" />
     </ActionButton>
   )
--- electron/note-mark/src/renderer/src/components/Button/NewNoteButton.tsx
+++ electron/note-mark/src/renderer/src/components/Button/NewNoteButton.tsx
@@ -1,9 +1,17 @@
 import { ActionButton, ActionButtonProps } from '@/components'
+import { createEmptyNoteAtom } from '@/store'
+import { useSetAtom } from 'jotai'
 import { LuFilePen } from 'react-icons/lu'
 
 export const NewNoteButton = ({ ...props }: ActionButtonProps) => {
+  const createEmptyNote = useSetAtom(createEmptyNoteAtom)
+
+  const handleCreation = () => {
+    createEmptyNote()
+  }
+
   return (
-    <ActionButton {...props}>
+    <ActionButton {...props} onClick={handleCreation}>
       <LuFilePen className="w-4 h-4 to-zinc-300" />
     </ActionButton>
   )
