19755c53037b6681e3a9437dfae9a6424e0177f7
fruitsdrink <fruitsdrink@126.com>
2025-04-17 15:31:40 +0800

    feat: blog-turborepo 完成登录接口

---
+++ turborepo/blog-turborepo/apps/api/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,28 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthJwtPayload } from '../types/auth-jwtPayload';
+import { AuthService } from '../auth.service';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly authService: AuthService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: AuthJwtPayload): Promise<unknown> {
+    const { sub: userId } = payload;
+    console.log('authService: ', this.authService);
+    const user = await this.authService.validateJwtUser(userId);
+    return user;
+  }
+}
