cd1bed0e7f03f64b4d3b470c52f75206ab1d1ad6
fruitsdrink <fruitsdrink@126.com>
2025-04-24 23:35:21 +0800

    fix: 解决安装electron依赖失败问题

--- electron/base/main.js
+++ electron/base/main.js
@@ -1,17 +1,38 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
-app.whenReady().then(() => {
+const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 300,
     height: 300,
     alwaysOnTop: true,
     x: 1500,
     y: 100,
+    frame: false,
+    transparent: true
   });
 
   // 如果是开发模式， 打开调试工具
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
+
+  // 设置窗口的纵横比为1:1
+  mainWindow.setAspectRatio(1);
 
   mainWindow.loadFile(path.resolve(__dirname, "index.html"));
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
--- electron/base/renderer.js
+++
@@ -1,3 +0,0 @@
-console.log("Hello World");
-
-document.querySelector("body").innerHTML = "<h1>Hello World111</h1>";
