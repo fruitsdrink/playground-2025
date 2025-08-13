9e0f6dd12408bd21a68a9051a524dbe88dc1e159
fruitsdrink <fruitsdrink@126.com>
2025-08-13 21:50:54 +0800

    feat: project-react-beginner

---
+++ react/project-react-beginner/src/components/ListingList.jsx
@@ -0,0 +1,15 @@
+import ListingCard from './ListingCard'
+
+const ListingList = ({listings})=>{
+  return <div className="flex flex-wrap justify-center gap-4">
+    {listings.length > 0 ? (
+      listings.map(listing => (
+        <ListingCard key={listing.id} listing={listing} />
+      ))
+    ):(
+      <p>No <legend></legend>istings found.</p>
+    )}
+  </div>
+}
+
+export default ListingList
